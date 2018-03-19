8e322a44ccf4c38f1085a6163e9f17862042f2ed
Obijuan <juan@iearobotics.com>
2018-03-19 15:51:44 +0100

    Prueba de subida a github 2

--- prueba1.py
+++ prueba1.py
@@ -1,6 +1,9 @@
 print("Mi primer programa en pycharm")
 print("Practiquemos con un bucle")
+print("Practiquemos con un bucle2")
+
 
 for i in range(5):
     print("Linea: {}".format(i))
     print("  Probando....")
+
